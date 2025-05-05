9a0268e2c1115dce43337f44465c1058fdd7fcf9
walaa soudy <77520251+walaasoudy@users.noreply.github.com>
2025-05-05 11:13:52 +0200

    Update .gitignore and improve logging messages; add new environment files

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const DBConnection = () => {
     mongoose.connect(process.env.DB_URL)
-        .then(() => console.log(`DB connection successful!`))
+        .then(() => console.log(`Database connection successful!`))
         .catch(err => console.error('DB connection error:', err));
 }
 
